b466ff043b730d88c0926cc7f68d4012d579d046
sairaj mote <sairajmote3@gmail.com>
2022-09-05 01:31:35 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const db = Gun(['http://localhost:8765/gun', 'https://gun-manhattan.herokuapp.com/gun']);
+const messageContainer = document.getElementById('message_container')
+db.get('messages').map().once((data) => {
+  if (data) {
+    const messageBox = document.createElement('div')
+    messageBox.textContent = data
+    messageContainer.append(messageBox)
+  }
+})
+console.log(db.get('messages'))
+const getMessage = document.getElementById('get_message')
+document.getElementById('my_form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (getMessage.value.trim() !== '') {
+    const index = new Date().toISOString()
+    db.get('messages').get(index).put(getMessage.value)
+    getMessage.value = ''
+  }
+})
