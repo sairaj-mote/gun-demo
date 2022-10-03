abd7e8c12a6d189ff33e09d7d93bd70f1338300f
sairaj mote <sairajmote3@gmail.com>
2022-10-03 21:08:00 +0530

    added editor js

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
-const db = Gun({ peers: ['http://localhost:8765/gun', 'https://gundm.herokuapp.com/gun'], localStorage: false });
+const db = Gun({ peers: ['http://localhost:8765/gun', 'https://gundm.herokuapp.com/gun'] });
 const messageContainer = document.getElementById('message_container')
-db.get('messages').map().once((data) => {
+let hasData
+db.get('articles').get('recentArticles').map().once((data) => {
   if (data) {
-    const messageBox = document.createElement('div')
-    messageBox.textContent = data
-    messageContainer.append(messageBox)
-  }
-})
-console.log(db.get('messages'))
-const getMessage = document.getElementById('get_message')
-document.getElementById('my_form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (getMessage.value.trim() !== '') {
-    const index = new Date().toISOString()
-    db.get('messages').get(index).put(getMessage.value)
-    getMessage.value = ''
+    hasData = JSON.parse(data)
+    // received data
   }
 })
 
+window.editor = new EditorJS({
+  holder: 'editorjs',
+  autofocus: true,
+  placeholder: 'Let`s write an awesome story!',
+  tools: {
+    header: {
+      class: Header,
+      config: {
+        placeholder: 'Header'
+      }
+    },
+    list: List,
+    image: SimpleImage,
+    quote: Quote,
+  },
+  data: hasData || {
+    blocks: [
+      {
+        type: 'header',
+        data: {
+          text: '',
+          level: 1
+        }
+      },
+    ]
+  },
+});
 
+document.getElementById('save').addEventListener('click', (e) => {
+  const randomInt = 'first'
+  editor.save().then((outputData) => {
+    db.get('articles').get('recentArticles').set(JSON.stringify(outputData))
+  }).catch((error) => {
+    console.log('Saving failed: ', error)
+  });
+})
